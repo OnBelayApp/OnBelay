2b37daf9f34b7eecaa594a2e6eda3279135742a8
Brian Tedder <imbrianted@gmail.com>
2016-01-07 12:29:01 -0700

    refactor onto auth.controller

--- server/controllers/api/auth.controller.js
+++ server/controllers/api/auth.controller.js
@@ -1,4 +1,5 @@
-var User = require('../models');
+var User = require('../models'),
+    jwt = require('jsonwebtoken');
 
 var createToken = function(user) {
  return jwt.sign({ user: user.username }, credentials.authentication.tokenSecret, {
@@ -21,6 +22,34 @@ module.exports = {
         });
       } else {
         res.json({'success': false, 'reason': 'Username does not exist'});
+      }
+    });
+  },
+  signUp: function(req, res) {
+
+    User.findOne({ req.body.username }, function(err, user) {
+      if (err) console.error(err);
+
+      if (user) {
+        res.json({
+          success: false,
+          reason: 'User with that username already exists'
+        });
+      } else {
+        var newUser = new User({
+          username: req.body.username,
+          password: null
+        });
+
+        newUser.hashPassword(req.body.password, function(hash) {
+          newUser.password = hash;
+          newUser.save(function(err, user) {
+            if (err) console.error(err);
+
+            var token = createToken(user);
+            res.json({ success: true, token: token });
+          })
+        })
       }
     });
   }
