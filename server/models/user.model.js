e8f1d987e2aef23b78e65071aca0ba778000b7c2
Robert Lin <Rlin415@gmail.com>
2016-01-06 20:49:21 -0800

    setup user schema

---
+++ server/models/user.model.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt-nodejs'),
+    ObjectId = mongoose.Schema.Types.ObjectId;
+
+var userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  password: String,
+  zipCode: Number,
+  skillLevel: String,
+  gender: String,
+  createdAt: Date,
+  updatedAt: Date
+});
+
+userSchema.pre('save', function(next) {
+  var now = new Date();
+  this.updated_at = now;
+  if (!this.created_at) {
+    this.created_at = now;
+  }
+  next();
+});
+
+userSchema.methods.hashPassword = function(password, callback) {
+  var hash = bcrypt.hash(password, null, null, function(err, hash) {
+    callback(hash);
+  });
+};
+
+userSchema.methods.comparePassword = function(attempt, hash, callback) {
+  bcrypt.compare(attempt, hash, function(err, res) {
+    callback(res);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
