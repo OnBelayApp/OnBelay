6223853583f5d8ebd658d58bbcb127dc726a0082
Ryan Jagger <ryanjagger@gmail.com>
2016-01-08 13:10:37 -0600

    Code cleanup

--- client/services/services.js
+++ client/services/services.js
@@ -8,7 +8,6 @@ angular.module('nova.services', [])
     }).catch(function(err){
       console.error(err);
     })
-
    };
 
   var signup = function(user){
@@ -17,15 +16,13 @@ angular.module('nova.services', [])
     }).catch(function(err){
       console.error(err);
     })
-
   };
 
 
+  var signout = function(){
+    
+  };
 
-
-
-
-  var signout = function(){};
 
   return {
       signin: signin,
