b3f76f222b6eba3e3f3e0b3d62749ae4feaa3b67
Ryan Jagger <ryanjagger@gmail.com>
2016-01-08 12:51:06 -0600

    Signup view/model/factory connected to backend

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -3,25 +3,25 @@ angular.module('nova.auth', [])
 .controller('AuthController', function ($scope, $window, $state, Auth) {
   $scope.user = {};
 
-  // $scope.signin = function () {
-  //   Auth.signin($scope.user)
-  //     .then(function (token) {
-  //       $window.localStorage.setItem('com.nova', token);
-  //       $state.go('main');
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
-  // };
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        // $window.localStorage.setItem('com.nova', token);
+        $state.go('main');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
-  // $scope.signup = function () {
-  //   Auth.signup($scope.user)
-  //     .then(function (token) {
-  //       $window.localStorage.setItem('com.nova', token);
-  //       $state.go('main');
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
-  // };
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        // $window.localStorage.setItem('com.nova', token);
+        $state.go('main');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 });
