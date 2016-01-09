7c37dc7f9d3e6c127f547a617c272cd0d0d857f3
Robert Lin <Rlin415@gmail.com>
2016-01-08 16:46:28 -0800

    add notification folder

---
+++ client/app/notifications/notifications.js
@@ -0,0 +1,36 @@
+angular.module('nova.notifications', [])
+
+  .controller('NotificationCtrl', function($scope, Notify) {
+    $scope.notifications = [];
+
+    $scope.getAllNotifications = function() {
+      Notify.fetchAllNotifications()
+        .then(function(res) {
+          $scope.notifications = res;
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+    }();
+
+    $scope.readAllNotifications = function() {
+      Notify.markAllNotificationsRead
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+    }();
+
+    $scope.climbOn = function(climber) {
+      Notify.replyToClimber(climber)
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+    };
+
+  });
