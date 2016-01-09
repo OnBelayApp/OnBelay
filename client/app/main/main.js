18a663322315feb3c1ba034347729fbc2d55cf31
Robert Lin <Rlin415@gmail.com>
2016-01-08 16:48:32 -0800

    define climbOn method

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,13 +1,26 @@
 angular.module('nova.main', [])
 
-.controller('MainController', function($scope, Climbers){
+.controller('MainController', function($scope, Climbers, Notify){
     $scope.activeClimbers = [];
+
     $scope.getActiveClimbers = function(){
       Climbers.getClimbers()
         .then(function(res) {
-          $scope.activeClimbers = res.data;
-        }, function(err) {
-          console.log(err);
+          $scope.activeClimbers = res;
+        })
+        .catch(function(err) {
+          console.error(err);
         });
     };
+
+    $scope.climbOn = function(climber) {
+      Notify.sendNotification(climber)
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+    };
+
 });
