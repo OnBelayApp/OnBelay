facca49a5c3d3eaba3ac90e035dfafa35adabafb
Brian Tedder <imbrianted@gmail.com>
2016-01-10 13:26:04 -0700

    request for all climbers no longer returns current user

--- server/controllers/api/user.controller.js
+++ server/controllers/api/user.controller.js
@@ -2,9 +2,14 @@ var User = require('../../models').User;
 
 module.exports = {
   findActiveClimbers: function(req, res) {
+    var authUser = req.decoded.user;
+
     User.find({climb: true}, function(err, climbers) {
       if (err) console.error(err);
       var result = climbers.map(function(climber) {
+        
+        if (climber.username === authUser) return;
+
         return {
           username: climber.username,
           first: climber.name.first,
