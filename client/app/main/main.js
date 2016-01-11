118d962c91fc297f26da871365de1c4ca7c3d96a
Brian Tedder <imbrianted@gmail.com>
2016-01-10 20:10:30 -0700

    status toggle changes based on status

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,7 +1,8 @@
 angular.module('nova.main', [])
 
-.controller('MainController', function($scope, Climbers, Notify){
+.controller('MainController', function($scope, Climbers, Notify, Auth){
     $scope.activeClimbers = [];
+    $scope.status = false;
 
     $scope.getActiveClimbers = function(){
       Climbers.getClimbers()
@@ -12,6 +13,19 @@ angular.module('nova.main', [])
           console.error(err);
         });
     }();
+
+    $scope.getStatus = function() {
+      Climbers.getStatus().then(function(res) {
+        $scope.status = res.status;
+        console.log($scope.status);
+      });
+    }();
+
+    $scope.updateStatus = function() {
+      Climbers.updateStatus().then(function(res) {
+        console.log(res);
+      });
+    };
 
     $scope.climbOn = function(climber) {
       Notify.sendNotification(climber)
