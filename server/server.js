3ad5b6201fdefff149b965e1c468c7056ee49afd
Brian Tedder <imbrianted@gmail.com>
2016-01-11 12:21:59 -0700

    db deploy

--- server/server.js
+++ server/server.js
@@ -13,11 +13,12 @@ app.use(express.static(__dirname + '/../client'));
 
 app.set('port', process.env.PORT || 3000);
 
-mongoose.connect('mongodb://localhost/onbelay');
-
 /* seeds the db with fake users for development */
 if (process.env.NODE_ENV === undefined) {
+  mongoose.connect('mongodb://localhost/onbelay');
   require('../fakeUsers')();
+} else {
+  mongoose.connect(process.env.MONGOLAB_URI);
 }
 
 controllers.climbOn(app);
