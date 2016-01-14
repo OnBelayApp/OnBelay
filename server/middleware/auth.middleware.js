608534e5203c647c203789f02a28ca68763e9041
Robert Lin <Rlin415@gmail.com>
2016-01-14 11:04:53 -0800

    change status codes

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -11,13 +11,13 @@ module.exports = function(req, res, next) {
   var token = req.headers['x-access-token'];
 
    if (!token) {
-     return res.status(403).json({ success: false, reason: 'Invalid token' });
+     return res.status(401).json({ success: false, reason: 'Invalid token' });
    }
 
   jwt.verify(token, tokenSecret, function(err, decoded) {
     if (err) {
       console.log(err);
-      return res.status(403).json({ success: false, reason: 'Invalid token' });
+      return res.status(401).json({ success: false, reason: 'Invalid token' });
     }
     req.decoded = decoded;
     next();
